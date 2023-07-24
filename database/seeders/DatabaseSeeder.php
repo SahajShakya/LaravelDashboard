809284e1857db3b8509fae082785fc873d2887c7
Sahaj Shakya <saz.shakya@gmail.com>
2023-07-24 21:58:25 +0545

    Port change

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use http\Client\Curl\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -19,5 +20,12 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         \App\Models\User::factory(50)->create();
+//        $user = User::factory()->create([
+//            'name'=>'Sahaj Shakya',
+//            'email'=>'saz@sahaj.com'
+//        ]);
+//        User::factory(6)->create([
+//            'user_id'=>$user->id
+//        ]);
     }
 }
